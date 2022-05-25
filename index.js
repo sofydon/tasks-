cde767dc3baaaf11b438fd642f151e43f7a7d924
sofydon <petinova@bk.ru>
2022-05-25 14:10:34 -0400

    add 2 more tasks

--- index.js
+++ index.js
@@ -31,20 +31,56 @@ firstTask();
 function secondTask() {
     // при помощи цикла for вывести от 20 до 10 в обратном порядке. когда цикл дойдет до 13 - остановить весь цикл
 
-    for (let i = 20; i >=10; i--) {
-        if (i === 13){
-            break;
-        }
-        console.log(i);
+    for (let i = 20; i > 10; i--) {
+        if (i === 13) break; {
+            console.log(i);
+        }    
     }
 }
 secondTask();
 
 function thirdTask() {
     // при помощи цикла for выведите все четные числа от 2 до 10 включительно
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 2; i <= 10; i++) {
         if (i % 2) continue;
         console.log(i);
     }
 }
 thirdTask();
+
+function fourthTask() {
+    // переписать код с for на while:
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+    let i = 2;
+    while (i < 16) {
+        i ++;
+        if (i % 2 === 0) {
+            continue;
+            } else {
+            console.log(i);
+        }
+    }
+}
+fourthTask();
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    // заполнить массив цифрами от 5 до 10.
+    for (let i = 5; i <= 10; i++) {
+        arrayOfNumbers [i - 5] =i;
+    }
+    console.log(arrayOfNumbers);
+    
+    // Не трогаем
+    return arrayOfNumbers;
+}
+fifthTask();
+
