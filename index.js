401b97a133834e483cfdde47f594ab734e371b5f
sofydon <petinova@bk.ru>
2022-05-26 14:18:09 -0400

    edit nasted loop

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ for (let i = 0; i <= lines; i++) {
     for (let j = 0; j < lines - i; j++) {
         res += " ";
     }
-    for (let j = 0; j < 2*i-1; j++) {
+    for (let j = 0; j < 2 * i + 1; j++) {
         res +="*";
     }
     res += '\n';
