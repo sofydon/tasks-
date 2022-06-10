bda2101cf96f7f420b5e8e6925c4b9bc10fc9bb9
sofydon <petinova@bk.ru>
2022-06-10 14:25:29 -0400

    functions

---
+++ functions.js
@@ -0,0 +1,10 @@
+function sayHello(name) {
+    return `Привет, ${name}!`;
+}
+console.log(sayHello('Антон'));
+
+
+function returnNeighboringNumbers(n) {
+    return [n-1, n, n+1];
+}
+console.log(returnNeighboringNumbers(4));
