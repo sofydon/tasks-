8627eeb98ebcbc7eb3952f95e56d4b46ba85c6dd
sofydon <petinova@bk.ru>
2022-06-18 21:26:13 -0400

    abbrevName func

--- script.js
+++ script.js
@@ -146,7 +146,23 @@ function solve(s){
 }
 console.log(solve('CODewar'));
 
+/* Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+The output should be two capital letters with a dot separating them.
+It should look like this: Sam Harris => S.H */
+//first 
+function abbrevName(name){
+    let result = name.split(" ").map(i => {
+        return i[0].toUpperCase();
+    });
 
+    return result.join(".");
+}
+console.log(abbrevName('sofia petinova'));
+
+//second
+function abbrevName(name){
+    return name.split(' ').map(i => i[0].toUpperCase()).join('.');
+}
 
 
 
