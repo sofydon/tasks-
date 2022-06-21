e96ac7b7e09e61e5ebee9cda8790f5a00be914e6
sofydon <petinova@bk.ru>
2022-06-20 22:04:22 -0400

    correct func

--- script.js
+++ script.js
@@ -154,15 +154,29 @@ function abbrevName(name){
     let result = name.split(" ").map(i => {
         return i[0].toUpperCase();
     });
-
     return result.join(".");
 }
 console.log(abbrevName('sofia petinova'));
+
 
 //second
 function abbrevName(name){
     return name.split(' ').map(i => i[0].toUpperCase()).join('.');
 }
+
+/* Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and
+stored on a computer. When documents (especially pretty old ones written with a typewriter), are digitised character
+recognition softwares often make mistakes.
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1 */
+
+function correct(string) { 
+    return string.replace(/5/g, 'S').replace(/0/g, 'O').replace(/1/g, 'I');
+}
+console.log(correct("L0ND0N"));
+console.log(correct('DUBL1N'));
 
 
 
