c1acdfa160b0341af332c553bb988e8532d8ad60
sofydon <petinova@bk.ru>
2022-06-22 21:17:39 -0400

    if else statement

--- script.js
+++ script.js
@@ -197,6 +197,22 @@ function noSpace(x){
 }
 console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
 
+/* Check to see if a string has the same amount of 'x's and 'o's. 
+The method must return a boolean and be case insensitive. The string can contain any char.*/
+function XO(str) {
+    let x = 0;
+    let o = 0;
+    for(let i = 0; i < str.length; i++) {
+        if (str[i].toLowerCase() === 'x') {
+            x++;
+        } else if (str[i].toLowerCase() === 'o') {
+            o++;
+        }
+    }
+    return x == o; 
+}
+console.log(XO("xooxx"));
+console.log(XO("xxOo"));
 
 
 
