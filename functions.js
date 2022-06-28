fb0671804673f9b8842c3922bc0b503f4a8057b7
sofydon <petinova@bk.ru>
2022-06-27 21:42:42 -0400

    recursion

--- functions.js
+++ functions.js
@@ -185,3 +185,25 @@ const recordCollection = {
   }
   
   console.log(lookUpProfile("Kristian", "lastName"));
+
+  function countdown(n){
+    if (n < 1) {
+      return [];
+    } else {
+      const countArray = countdown(n - 1);
+      countArray.unshift(n);
+      return countArray;
+    }
+  }
+  console.log(countdown(5));
+
+  function rangeOfNumbers(startNum, endNum){
+    if(endNum - startNum === 0){
+       return [endNum];
+    } else {
+      const range = rangeOfNumbers(startNum, endNum - 1);
+      range.push(endNum);
+      return range;
+    }
+  }
+  console.log(rangeOfNumbers(2, 10));
