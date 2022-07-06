0ccc1b12f5fe054b789ef0d802b6b07339691e71
sofydon <petinova@bk.ru>
2022-07-06 00:00:20 -0400

    for in tasks

--- script.js
+++ script.js
@@ -372,3 +372,19 @@ function grabDoll(dolls){
     return bag;
 }
 console.log(grabDoll(["Mickey Mouse","Barbie doll","Hello Kitty","Hello Kitty","Hello Kitty","Snow white"]));
+
+/* Coding in function giveMeFive, function accept 1 parameter:obj, it's an object.
+You need to the traverse the obj, if the length of the object key equals to 5, then push the key value
+to the array (you need to define the array by yourself, this time I won't help you). Additionally push the value
+to the array as well, if the length of the value is equal to 5.*/
+function giveMeFive(obj){
+    let arr = [];
+    for(let key in obj) {
+        if(key.length == 5) {arr.push(key);}
+        if(obj[key].length == 5) {arr.push(obj[key]);}
+    }
+    return arr;
+}
+console.log(giveMeFive({Ihave:"enough", money:"to",buy:"a",car:"model"}));
+console.log(giveMeFive({Our:"earth",is:"a",beautyful:"world"}));
+console.log(giveMeFive({Pears:"than",apple:"sweet"}));
